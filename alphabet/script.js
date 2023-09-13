f649d2241b9275c8a4e55d2ee2bb8c9927ec00b8
computer_chris <computergeek99@gmail.com>
2023-09-13 09:46:35 -0700

    alphabet page progress

---
+++ alphabet/script.js
@@ -0,0 +1,41 @@
+const dom_app = document.querySelector('#app');
+const img_arr = [
+    'a', 'Apple','b', 'Ball','c', 'Cat','d', 'Dog',
+    'e', 'Egg','f', 'Fish','g', 'Goat','h', 'Heart',
+    'i', 'Igloo','j', 'Jellyfish','k', 'Kite',
+    'l', 'Llama','m', 'Mario','n', 'Nose','o', 'Orange',
+    'p', 'Pig','q', 'Queen','r', 'Rabbit','s', 'Sun',
+    't', 'Tree','u', 'Umbrella','v', 'Violin',
+    'w', 'Whale','x', 'X-Ray','y', 'Yarn','z', 'Zebra'
+];
+
+for(let i = 65; i <= 90; i++) {
+    const letter_upper = String.fromCharCode(i);
+    const letter_lower = letter_upper.toLowerCase();
+    const letter_index = img_arr.indexOf(letter_lower);
+    const word = img_arr[(letter_index + 1)];
+    const img_src = './img/' + letter_lower + '_' + word.toLowerCase() + '.png';
+    const div = document.createElement('div');
+    div.classList.add('abc_cell');
+    const div_letter = document.createElement('div');
+    div_letter.classList.add('abc_letter');
+    const div_word = document.createElement('div');
+    div_word.classList.add('abc_word');
+    const img = document.createElement('img');
+    div.append(div_letter, img, div_word);
+    div_letter.textContent = letter_upper + ' ' + letter_lower;
+    div_word.textContent = word;
+    img.setAttribute('src', img_src);
+    img.addEventListener('click', evt => {
+        img.classList.remove('img_base');
+        img.classList.add('img_click');
+    });
+
+    img.addEventListener('transitionend', evt => {
+        if(evt.propertyName !== 'transform') return;
+        img.classList.add('img_base');
+        img.classList.remove('img_click');
+    })
+    
+    dom_app.appendChild(div);
+}
---
+++ draw/script.js
@@ -0,0 +1,131 @@
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+let isDrawing = false;
+let x = 0;
+let y = 0;
+let offsetX;
+let offsetY;
+const ongoingTouches = [];
+
+
+function initiateCanvas() {
+
+    canvas.addEventListener('touchstart', drawStart);
+    canvas.addEventListener('touchend', drawEnd);
+    canvas.addEventListener('touchcancel', drawCancel);
+    canvas.addEventListener('touchmove', drawMove);
+    canvas.addEventListener('mousedown', e => {
+        x = e.offsetX;
+        y = e.offsetY;
+        isDrawing = true;
+    });
+
+    canvas.addEventListener('mousemove', e => {
+        if(!isDrawing) return;
+        
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+        
+    });
+
+    canvas.addEventListener('mouseup', (e) => {
+        if(!isDrawing) return;
+
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    });
+
+}
+
+function drawStart(evt) {
+    evt.preventDefault();
+    const touches = evt.changedTouches;
+    offsetX = canvas.getBoundingClientRect().left;
+    offsetY = canvas.getBoundingClientRect().top;
+    for (let i = 0; i < touches.length; i++) {
+        ongoingTouches.push(copyTouch(touches[i]));
+    }
+}
+
+function drawMove(evt) {
+    evt.preventDefault();
+    const touches = evt.changedTouches;
+    for (let i = 0; i < touches.length; i++) {
+        const color = document.getElementById('selColor').value;
+        const idx = ongoingTouchIndexById(touches[i].identifier);
+        if (idx >= 0) {
+            context.beginPath();
+            context.moveTo(ongoingTouches[idx].clientX - offsetX, ongoingTouches[idx].clientY - offsetY);
+            context.lineTo(touches[i].clientX - offsetX, touches[i].clientY - offsetY);
+            context.lineWidth = document.getElementById('selWidth').value;
+            context.strokeStyle = color;
+            context.lineJoin = "round";
+            context.closePath();
+            context.stroke();
+            ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+        }
+    }
+}
+
+function drawEnd(evt) {
+    evt.preventDefault();
+    const touches = evt.changedTouches;
+    for (let i = 0; i < touches.length; i++) {
+        const color = document.getElementById('selColor').value;
+        let idx = ongoingTouchIndexById(touches[i].identifier);
+        if (idx >= 0) {
+            context.lineWidth = document.getElementById('selWidth').value;
+            context.fillStyle = color;
+            ongoingTouches.splice(idx, 1);  // remove it; we're done
+        }
+    }
+}
+
+function drawCancel(evt) {
+    evt.preventDefault();
+    const touches = evt.changedTouches;
+    for (let i = 0; i < touches.length; i++) {
+        let idx = ongoingTouchIndexById(touches[i].identifier);
+        ongoingTouches.splice(idx, 1);  // remove it; we're done
+    }
+}
+
+function copyTouch({ identifier, clientX, clientY }) {
+    return { identifier, clientX, clientY };
+}
+
+function ongoingTouchIndexById(idToFind) {
+    for (let i = 0; i < ongoingTouches.length; i++) {
+        const id = ongoingTouches[i].identifier;
+        if (id === idToFind) {
+            return i;
+        }
+    }
+    return -1;    // not found
+}
+
+function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = document.getElementById('selColor').value;
+    context.lineWidth = document.getElementById('selWidth').value;
+    context.lineJoin = "round";
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.closePath();
+    context.stroke();
+}
+
+function clearCanvas() {
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+// Resize the canvas
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+// Load drawing events
+document.querySelector('#canvas-button-clear').addEventListener('click', evt => clearCanvas());
+initiateCanvas();
